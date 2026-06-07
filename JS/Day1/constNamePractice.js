2824ff56a99a0327f7d827146a611214a3877516
Faitha-begam <faithabegam078@gmail.com>
2026-06-08 00:34:32 +0530

    Enhanced index file.

---
+++ JS/Day1/constNamePractice.js
@@ -0,0 +1,89 @@
+const eagleNest;
+console.log(eagleNest);
+
+const volcanoCrater;
+console.log(volcanoCrater);
+
+const spiderWeb;
+console.log(spiderWeb);
+
+const crystalCave;
+console.log(crystalCave);
+
+const honeyJar;
+console.log(honeyJar);
+
+const silverCoin;
+console.log(silverCoin);
+
+const pirateShip;
+console.log(pirateShip);
+
+const junglePath;
+console.log(junglePath);
+
+const cometTail;
+console.log(cometTail);
+
+const mapleLeaf;
+console.log(mapleLeaf);
+
+const waterfallEdge;
+console.log(waterfallEdge);
+
+const marbleStatue;
+console.log(marbleStatue);
+
+const dolphinFin;
+console.log(dolphinFin);
+
+const cactusFlower;
+console.log(cactusFlower);
+
+const pearlNecklace;
+console.log(pearlNecklace);
+
+const wizardTower;
+console.log(wizardTower);
+
+const velvetCurtain;
+console.log(velvetCurtain);
+
+const snowflakePattern;
+console.log(snowflakePattern);
+
+const lighthouseBeam;
+console.log(lighthouseBeam);
+
+const acornTree;
+console.log(acornTree);
+
+const emeraldStone;
+console.log(emeraldStone);
+
+const harvestMoon;
+console.log(harvestMoon);
+
+const falconFeather;
+console.log(falconFeather);
+
+const canyonWall;
+console.log(canyonWall);
+
+const violinString;
+console.log(violinString);
+
+const penguinColony;
+console.log(penguinColony);
+
+const orchidPetal;
+console.log(orchidPetal);
+
+const canyonRiver;
+console.log(canyonRiver);
+
+const mosaicTile;
+console.log(mosaicTile);
+
+const iglooEntrance;
+console.log(iglooEntrance);
